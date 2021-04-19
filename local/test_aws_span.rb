5b6cb54b8747c42fa75da3c79b04b1940bec865c
Assaf Paneth <assaf.paneth@gmail.com>
2021-04-19 17:59:43 +0300

    take local tests out of version control

--- local/test_aws_span.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'aws-sdk-s3'
-require './lib/epsagon'
-
-Epsagon.init(metadata_only: false, debug: true, backend: 'localhost:4569/', app_name: 'test-aws-sdk-s3')
-
-print Aws::S3::Client.new.get_object(bucket: 'epsagon-ruby-test', key: 'test_file.txt').body.read
--- local/test_net_http_span.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require './lib/epsagon'
-require 'net/http'
-
-Net::HTTP.post(
-  URI('http://example.com/some/path/index.html;pathparam=who,even,uses,this?q1=a&q2=b&q1=c'),
-  { amir: :asdasd }.to_json
-)
