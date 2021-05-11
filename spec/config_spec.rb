355565dfb090a7d99f78491a956961034df446df
Jan Schulte <jan@epsagon.com>
2021-05-11 18:28:29 -0400

    Add specs to ensure behavior for overwriting keys
    When the user has environment variables specified *and* provides a token
    and app name via `Epsagon.init`

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -86,6 +86,54 @@ RSpec.describe do
           expect(Epsagon.get_config[:backend]).to eq 'opentelemetry.tc.epsagon.com:443/traces'
         end
       end
+
+      context 'with token and app_name' do
+        before do
+          Epsagon.init(token: epsagon_token, app_name: epsagon_app_name)
+        end
+
+        it 'assigns the correct token' do
+          expect(Epsagon.get_config[:token]).to eq epsagon_token
+        end
+
+        it 'assigns the correct app_name' do
+          expect(Epsagon.get_config[:app_name]).to eq epsagon_app_name
+        end
+      end
+
+      context 'with environment variable set' do
+        let(:second_epsagon_token) { 'second_token' }
+        let(:second_epsagon_app_name) { 'second_app_name' }
+
+        before do
+          Epsagon.init(token: second_epsagon_token, app_name: second_epsagon_app_name)
+        end
+
+        around do |example|
+          ClimateControl.modify EPSAGON_TOKEN: epsagon_token,
+            EPSAGON_APP_NAME: epsagon_app_name,
+            EPSAGON_DEBUG: epsagon_debug.to_s,
+            EPSAGON_METADATA: epsagon_metadata.to_s do
+              example.run
+          end
+        end
+
+        it 'does overwrite the environment epsagon_token' do
+          expect(Epsagon.get_config[:token]).to eq second_epsagon_token
+        end
+
+        it 'does overwrite the environment epsagon_app_name' do
+          expect(Epsagon.get_config[:app_name]).to eq second_epsagon_app_name
+        end
+
+        it 'keeps the debug flag' do
+          expect(Epsagon.get_config[:debug]).to eq epsagon_debug
+        end
+
+        it 'keeps the metadata flag' do
+          expect(Epsagon.get_config[:metadata_only]).to eq epsagon_metadata
+        end
+      end
     end
   end
 end
