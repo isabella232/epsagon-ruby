4419e55a0766bb7deec21c56c39f704153d18def
Jan Schulte <jan@epsagon.com>
2021-06-08 14:53:10 -0400

    Update Rails Spec

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
+
 require 'rails'
 require 'rack/test'
 require 'opentelemetry/sdk'
 require 'byebug'
+require 'webmock/rspec'
+require 'climate_control'
 require_relative '../lib/instrumentation/rails'
 require_relative 'test_helpers/app_config'
 
@@ -34,64 +37,7 @@ describe 'Rails Instrumentation' do
     ::Rails.application = default_rails_app
   end
 
-  it 'sets the span name to ControllerName#action' do
-    get '/ok'
-
-    expect(last_response.body).to eq 'actually ok'
-    expect(last_response.ok?).to eq true
-    expect(span.name).to eq 'example.org'
-    expect(span.kind).to eq :server
-    expect(span.status.ok?).to eq true
-
-    # expect(span.instrumentation_library.name).to eq 'OpenTelemetry::Instrumentation::Rack'
-    # expect(span.instrumentation_library.version).to eq OpenTelemetry::Instrumentation::Rack::VERSION
-
-    # expect(span.attributes['http.method']).to eq 'GET'
-    # expect(span.attributes['http.host']).to eq 'example.org'
-    expect(span.attributes['http.scheme']).to eq 'http'
-    # expect(span.attributes['http.target']).to eq '/ok'
-    expect(span.attributes['http.status_code']).to eq 200
-    expect(span.attributes['http.user_agent']).to eq nil
-    expect(span.attributes['http.route']).to eq nil
-  end
-
-  it 'sets the span name when the controller raises an exception' do
-    get 'internal_server_error'
-
-    expect(span.name).to eq 'example.org'
-  end
-
-  it 'does not set the span name when an exception is raised in middleware' do
-    get '/ok?raise_in_middleware'
-
-    expect(span.name).to eq 'example.org'
-  end
-
-  it 'has the correct span name' do
-    get '/ok?redirect_in_middleware'
-
-    expect(span.name).to eq 'example.org'
-  end
-
-  describe 'when the application has exceptions_app configured' do
-    let(:rails_app) { AppConfig.initialize_app(use_exceptions_app: true) }
-
-    it 'has the correct span name' do
-      get 'internal_server_error'
-
-      expect(span.name).to eq 'example.org'
-    end
-  end
-
-  describe 'when the application has enable_rails_route enabled' do
-    # before do
-    #   OpenTelemetry::Instrumentation::Rails::Instrumentation.instance.config[:enable_recognize_route] = true
-    # end
-
-    # after do
-    #   OpenTelemetry::Instrumentation::Rails::Instrumentation.instance.config[:enable_recognize_route] = false
-    # end
-
+  describe 'Span Format' do
     it 'sets the span name to ControllerName#action' do
       get '/ok'
 
@@ -110,22 +56,73 @@ describe 'Rails Instrumentation' do
       # expect(span.attributes['http.target']).to eq '/ok'
       expect(span.attributes['http.status_code']).to eq 200
       expect(span.attributes['http.user_agent']).to eq nil
-      # expect(span.attributes['http.route']).to eq '/ok(.:format)'
+      expect(span.attributes['http.route']).to eq nil
     end
-  end
 
-  #
-  # Not sure if we need this test case because right now we're not installing EpsagonRackInstrumentation
-  #
-  skip describe 'when the application does not have the tracing rack middleware' do
-    let(:rails_app) { AppConfig.initialize_app(remove_rack_tracer_middleware: true) }
+    it 'sets the span name when the controller raises an exception' do
+      get 'internal_server_error'
 
-    it 'does something' do
-      get '/ok'
+      expect(span.name).to eq 'example.org'
+    end
 
-      expect(last_response.body).to eq 'actually ok'
-      expect(last_response.ok?).to eq true
-      expect(spans.size).to eq(0)
+    it 'does not set the span name when an exception is raised in middleware' do
+      get '/ok?raise_in_middleware'
+
+      expect(span.name).to eq 'example.org'
+    end
+
+    it 'has the correct span name' do
+      get '/ok?redirect_in_middleware'
+
+      expect(span.name).to eq 'example.org'
+    end
+
+    describe 'when the application has exceptions_app configured' do
+      let(:rails_app) { AppConfig.initialize_app(use_exceptions_app: true) }
+
+      it 'has the correct span name' do
+        get 'internal_server_error'
+
+        expect(span.name).to eq 'example.org'
+      end
+    end
+
+    describe 'when the application has enable_rails_route enabled' do
+      it 'sets the span name to ControllerName#action' do
+        get '/ok'
+
+        expect(last_response.body).to eq 'actually ok'
+        expect(last_response.ok?).to eq true
+        expect(span.name).to eq 'example.org'
+        expect(span.kind).to eq :server
+        expect(span.status.ok?).to eq true
+
+        # expect(span.instrumentation_library.name).to eq 'OpenTelemetry::Instrumentation::Rack'
+        # expect(span.instrumentation_library.version).to eq OpenTelemetry::Instrumentation::Rack::VERSION
+
+        # expect(span.attributes['http.method']).to eq 'GET'
+        # expect(span.attributes['http.host']).to eq 'example.org'
+        expect(span.attributes['http.scheme']).to eq 'http'
+        # expect(span.attributes['http.target']).to eq '/ok'
+        expect(span.attributes['http.status_code']).to eq 200
+        expect(span.attributes['http.user_agent']).to eq nil
+        # expect(span.attributes['http.route']).to eq '/ok(.:format)'
+      end
+    end
+
+    #
+    # Not sure if we need this test case because right now we're not installing EpsagonRackInstrumentation
+    #
+    skip describe 'when the application does not have the tracing rack middleware' do
+      let(:rails_app) { AppConfig.initialize_app(remove_rack_tracer_middleware: true) }
+
+      it 'does something' do
+        get '/ok'
+
+        expect(last_response.body).to eq 'actually ok'
+        expect(last_response.ok?).to eq true
+        expect(spans.size).to eq(0)
+      end
     end
   end
 
